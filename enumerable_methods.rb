dbc770e4721ebaabd71a3e7b03fb62773ef24651
Mario Barrios <mariobarriossalazar@outlook.com>
2019-10-28 14:38:02 -0600

    Added my_select and my_all? methods with successful tests

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -22,10 +22,40 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    return to_enum unless block_given?
+
+    selected = []
+    my_each do |element|
+      result = yield(element)
+      next unless result
+
+      selected << element
+    end
+    selected
+  end
+
+  def my_all?
+    result = false
+    if block_given?
+      my_each do |element|
+        result = yield(element)
+      end
+    else
+      my_each do |element|
+        result = true unless element.nil? || element == false
+      end
+    end
+    result
+  end
 end
 
-test = [1, 2, 3, 4, 5]
-test2 = %w[test for each with index method]
+test_my_each = [1, 2, 3, 4, 5]
+test_my_each_with_index = %w[test for each with index method]
+test_my_select = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-test.my_each { |el| puts el }
-test2.my_each_with_index { |el, i| puts "Element = #{el} correspondign index = #{i}" }
+#test_my_each.my_each { |el| puts el }
+#test_my_each_with_index.my_each_with_index { |el, i| puts "Element = #{el} corresponding index = #{i}" }
+#print test_my_select.my_select { |element| element < 8 }
+#print %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
