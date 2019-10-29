6e4ff7b153f73f0f9817dd00d3f0445c65a6ef15
Mario Barrios <mariobarriossalazar@outlook.com>
2019-10-29 10:18:51 -0600

    Added my_any? and my_none? methods with successful tests

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -37,17 +37,45 @@ module Enumerable
   end
 
   def my_all?
-    result = false
     if block_given?
       my_each do |element|
         result = yield(element)
+        return false if result == false
       end
     else
       my_each do |element|
-        result = true unless element.nil? || element == false
+        return false if element.nil? || element == false
       end
     end
-    result
+    true
+  end
+
+  def my_any?
+    if block_given?
+      my_each do |element|
+        result = yield(element)
+        return true if result == true
+      end
+    else
+      my_each do |element|
+        return true if element == true
+      end
+    end
+    false
+  end
+
+  def my_none?
+    if block_given?
+      my_each do |element|
+        result = yield(element)
+        return false if result == true
+      end
+    else
+      my_each do |element|
+        return false if element == true
+      end
+    end
+    true
   end
 end
 
@@ -58,4 +86,20 @@ test_my_select = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #test_my_each.my_each { |el| puts el }
 #test_my_each_with_index.my_each_with_index { |el, i| puts "Element = #{el} corresponding index = #{i}" }
 #print test_my_select.my_select { |element| element < 8 }
-#print %w[ant bear cat].all? { |word| word.length >= 3 } #=> true
+# tests for my_all?
+#puts %w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+#puts %w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+#puts [nil, true, 99].my_all?                              #=> false
+#puts [].my_all?                                           #=> true
+# tests for my_any
+#puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+#puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> true
+#puts [nil, true, 99].my_any?                              #=> true
+#puts [].my_any?                                           #=> false
+# tests for my_none?
+#puts %w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+#puts %w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+#puts [].my_none?                                           #=> true
+#puts [nil].my_none?                                        #=> true
+#puts [nil, false].my_none?                                 #=> true
+#puts [nil, false, true].my_none?                           #=> false
